44e0b1077159376bc40ea1a4db4697e996ced67b
Shubham Lal <shubhamlal400@gmail.com>
2018-07-24 21:07:35 +0530

    Changes in the server.js'

--- server.js
+++ server.js
@@ -1,64 +1,56 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-var app = express();
-const port  = process.env.PORT || 3000;
 
-app.set('view engine','hbs');
-hbs.registerPartials(__dirname + "/views/partials");
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-	return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
-app.use((req,res,next) => {
-	var now = new Date().toString();
-	var log = `${now}:${req.method} ${req.url}`
-	console.log(log);
-	fs.appendFile('server.log',log + '\n',(err)=> {
-		if(err)
-		{
-			console.log('Unable to apped File in server.log');
-		}
-	});
-	next();
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
-app.get('/Home',(req,res) =>{
-	res.render('home.hbs',{
-		homePage:'This is Home page'
-	});
-});
-app.use(express.static(__dirname + '/public'));
-app.get('/',(req,res) =>{
-	res.send({
-		name:'Shubham',
-		likes:[
-		'Biking',
-		'Citing'
-		]
-	});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
-app.get('/about',(req,res) => {
-	res.render('about.hbs',{
-		pageTitle: 'About Page'
-	});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
-app.get('/bad', (req,res) => {
-	res.send({
-		errorMessage:'Unable to handle request'
-	});
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
-
-app.use((req,res,next) => {
-	res.render('maintenance.hbs');
-	//Comment for maintenance
-	next();
-});
-
-app.listen(port,() =>{
-	console.log(`Server is listening in the port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
